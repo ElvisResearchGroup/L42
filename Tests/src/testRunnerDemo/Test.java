e159a358ec87f30b92e662b6d478b580e033ff87
james <james@user-ThinkPad-X200>
2016-07-01 13:37:34 +1200

    Fix testRunnerDemo test name; re-break (as requested) TestRunner.

--- Tests/src/testRunnerDemo/Test.java
+++ Tests/src/testRunnerDemo/Test.java
@@ -7,11 +7,13 @@ import org.junit.Before;
 import org.junit.runners.Parameterized.Parameters;
 
 public class Test extends helpers.TestRunner{
-  @Parameters(name = "{index}:{0}")
+  @Parameters(name = "{index}:{1}")
   public static List<Object[]> go(){return goInner(
    //"testNameToRunOnlyOne",
    //Opt.NoDeplCurrent
    //,Opt.NoAss,Opt.NoTrust,
    //,Opt.DeplAT1,Opt.DeplAT2
+      Opt.AllTests,
+      Opt.NOP
     );}
 }
