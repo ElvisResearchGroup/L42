198b017786c69100a7a254b25c1d813437f0ff34
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2016-08-24 15:42:33 +0700

    Understanding testing setup

--- Tests/src/adamsTowel01/Test.java
+++ Tests/src/adamsTowel01/Test.java
@@ -21,12 +21,16 @@ public class Test extends helpers.TestRunner{
                        // (starting from one; as many times as you like; will not wait for towels)
       // Opt.Parallel,
       //// big individual deployment options
-       Opt.DeplAT1, // AdamsTowel01
+      // Opt.DeplAT1, // AdamsTowel01
       // Opt.DeplAT2, // AdamsTowel02
       // Opt.Project, // Run the local libProject as a folder, expecting it to deploy a project towel
       //// options for deploying small things
-      //"TestDebug.L42", // Name of a file in libTests; edit to match your file
-      // Opt.AllTests, // All files in libTests, as indivisrcdual tests, in no defined order
+      "TestDebug.L42", // Name of a file in libTests; edit to match your file
+      ///"TestParseAnnotations.L42",
+      //"TestParseBadDecoration.L42",
+      //"TestParseNoBinding.L42",
+      //"TestParseVector02.L42",
+      //Opt.AllTests, // All files in libTests, as indivisrcdual tests, in no defined order
       Opt.NOP  // Convenience option, so that all of the other options can end with a comma
     );
     return tests;
