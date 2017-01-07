b6f82563c32d68c7289576c9eb36eb74d2627849
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2017-01-07 13:48:45 +0700

    repl

---
+++ Tests/src/repl/ReplGui.java
@@ -0,0 +1,109 @@
+package repl;
+
+import java.awt.*; import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import javax.swing.*;
+
+import facade.L42;
+@SuppressWarnings("serial")
+public class ReplGui extends JFrame {
+ public static void main(String[] args) {
+ helpers.TestHelper.configureForTest();
+ SwingUtilities.invokeLater(()-> {
+ //UIManager.getLookAndFeelDefaults()
+ //.put("defaultFont", new Font("Arial", Font.PLAIN, 24));
+ ReplGui g = new ReplGui();
+ g.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+ g.getRootPane().setLayout(new BorderLayout());
+ g.runB = new JButton("------Bar------");
+ g.runB.addActionListener(new ActionListener() {
+ public void actionPerformed(ActionEvent e) {g.runCode();}});
+ g.getRootPane().add(g.runB, BorderLayout.SOUTH);
+ g.buildGui(g.getRootPane());
+ g.pack();
+ g.setVisible(true);
+ });
+ }
+
+JTextArea loadedSrc=new JTextArea(20, 50);
+JTextArea newSrc=new JTextArea(2, 50);
+JTextArea output=new JTextArea(20, 50);
+JTextArea errors=new JTextArea(20, 50);
+ReplState repl=null;
+BufferedReader err;
+boolean running=false;
+JButton runB;
+ExecutorService executor = Executors.newFixedThreadPool(1);
+void runCode(){
+  if(running){throw new Error("Was running");}
+  running=true;
+  runB.setText("Running");
+  runB.setEnabled(false);
+  /*Future<Object> future = */executor.submit(this::auxRunCode);
+  }
+void auxRunCode(){
+  try{
+  String code=newSrc.getText();
+  if(repl==null){ repl=ReplState.start("{"+code+"}");}
+  else{
+    ReplState newR=repl.add(code);
+    if(newR!=null){repl=newR;}
+    }
+  }
+  catch(Throwable t){
+     //somehow t.printstacktrace freeze stuff as well as inspecting t.cause
+      System.out.println(
+            ""+t+"\n"+
+           Arrays.asList(t.getStackTrace()).stream()
+           .map(e->e.toString()+"\n").reduce("",(a,b)->a+b));
+      }
+  finally{
+    SwingUtilities.invokeLater(()->{try{
+      output.setText(L42.record.toString());
+      String newErr="";
+      try {while(err.ready()){newErr+="\n"+err.readLine();}}
+      catch (IOException e) {throw new Error(e);}
+      errors.setText(errors.getText()+newErr);
+      if(repl==null){return;}
+      loadedSrc.setText(repl.originalS);
+      }
+      finally{
+        this.running=false;
+        runB.setEnabled(true);
+        runB.setText("Run!");
+      }});
+    }
+  }
+void buildGui(JRootPane pane){
+  JTabbedPane tabbedPane = new JTabbedPane();  
+  loadedSrc.setEditable(false);
+  output.setEditable(false);
+  errors.setEditable(false);
+  tabbedPane.addTab("new code", new JScrollPane(newSrc));
+  tabbedPane.addTab("loaded", new JScrollPane(loadedSrc));
+  tabbedPane.addTab("output", new JScrollPane(output));
+  tabbedPane.addTab("errors", new JScrollPane(errors));
+  newSrc.setFont(newSrc.getFont().deriveFont(40f));
+  PipedOutputStream pErr = new PipedOutputStream();   
+  System.setErr(new PrintStream(pErr));
+  System.setOut(new PrintStream(pErr));   
+  PipedInputStream pIn;try {pIn = new PipedInputStream(pErr);}
+  catch (IOException e) {throw new Error(e);}  
+  err = new BufferedReader(new InputStreamReader(pIn));
+  pane.add(tabbedPane,BorderLayout.CENTER);
+  }
+
+
+}
---
+++ Tests/src/repl/ReplState.java
@@ -0,0 +1,115 @@
+package repl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
+import ast.ErrorMessage;
+import ast.ExpCore;
+import ast.ExpCore.ClassB.Member;
+import ast.ExpCore.ClassB.NestedClass;
+import ast.Expression;
+import ast.Ast.Doc;
+import ast.Ast.Position;
+import ast.Ast.Stage;
+import ast.Expression.ClassB;
+import ast.Expression.ClassReuse;
+import coreVisitors.CloneVisitor;
+import facade.ErrorFormatter;
+import facade.L42;
+import facade.Parser;
+import facade.L42.ExecutionStage;
+import programReduction.ProgramReduction;
+import sugarVisitors.Desugar;
+import sugarVisitors.InjectionOnCore;
+
+public class ReplState {
+  String originalS;
+  ast.Expression.ClassReuse originalL;
+  ast.ExpCore.ClassB desugaredL;
+  public ReplState(String originalS, ast.Expression.ClassReuse originalL, ast.ExpCore.ClassB desugaredL) {
+    this.originalS = originalS;
+    this.originalL = originalL;
+    this.desugaredL = desugaredL;
+    }
+public static ReplState start(String code){
+  try{
+    Expression.ClassReuse code1=(ClassReuse) Parser.parse("Repl",code);
+    auxiliaryGrammar.WellFormedness.checkAll(code1);
+    Expression.ClassReuse code2=(ClassReuse)Desugar.of(code1);
+    assert auxiliaryGrammar.WellFormedness.checkAll(code2);
+    ExpCore.ClassB code3=(ExpCore.ClassB)code2.accept(new InjectionOnCore());
+    assert coreVisitors.CheckNoVarDeclaredTwice.of(code3);
+    // TODO: will die after new reduction Refresh of position identities, it is used to generate correct Java code.
+    code3=(ExpCore.ClassB)code3.accept(new CloneVisitor(){
+      @Override public ExpCore visit(ExpCore.ClassB cb){
+        Position p=cb.getP();
+        cb=cb.withP(new Position(p.getFile(),p.getLine1(),p.getPos1(),p.getLine2(),p.getPos2()));
+        return super.visit(cb);
+        }
+      });
+    ExpCore.ClassB result= ProgramReduction.allSteps(code3);
+    return new ReplState(code, code2,result);
+    }
+    catch(org.antlr.v4.runtime.misc.ParseCancellationException parser){
+      System.out.println(parser.getMessage());
+      return null;
+      }
+    catch(ErrorMessage msg){
+      ErrorFormatter.topFormatErrorMessage(msg);
+      return null;
+      }   
+  }
+  public ReplState add(String code){
+    Expression.ClassB cbEmpty=new ClassB(Doc.empty(),Doc.empty(),new ast.Ast.InterfaceHeader(),Collections.emptyList(), Collections.emptyList(), Collections.emptyList(),Position.noInfo, Stage.Star);
+    try{
+      //parse
+      Expression.ClassB codeTmp=(ClassB) Parser.parse("Repl","{"+code+"}");  
+      //new original
+      ClassReuse newOriginal = this.originalL;      
+      List<ast.Expression.ClassB.Member> newOriginalMs=newOriginal.getInner().getMs();
+      newOriginalMs.addAll(codeTmp.getMs());
+      newOriginal.withInner(newOriginal.getInner().withMs(newOriginalMs));
+      //new src to desugar
+      List<ClassB.Member>newMs=new ArrayList<>();
+      int nestedAdded=0;
+      for( Member m:this.desugaredL.getMs()){
+        if(!(m instanceof NestedClass)){continue;}
+        NestedClass nc=(NestedClass)m;
+        newMs.add(new ClassB.NestedClass(Doc.empty(),nc.getName(),cbEmpty,nc.getP()));
+        nestedAdded+=1;
+        }
+      newMs.addAll(codeTmp.getMs());
+      codeTmp=codeTmp.withMs(newMs);
+      Expression code2=Desugar.of(codeTmp);
+      ExpCore.ClassB code3=(ExpCore.ClassB)code2.accept(new InjectionOnCore());
+      // TODO: will die after new reduction Refresh of position identities, it is used to generate correct Java code.
+      code3=(ExpCore.ClassB)code3.accept(new CloneVisitor(){
+        @Override public ExpCore visit(ExpCore.ClassB cb){
+          Position p=cb.getP();
+          cb=cb.withP(new Position(p.getFile(),p.getLine1(),p.getPos1(),p.getLine2(),p.getPos2()));
+          return super.visit(cb);
+          }
+       });
+      //integrate new desugared src with old desugared code
+      List<Member> resultMs=new ArrayList<>(this.desugaredL.getMs());
+      for(int i=nestedAdded;i<code3.getMs().size();i++){
+        resultMs.add(code3.getMs().get(i));
+        }
+      code3=code3.withMs(resultMs);
+      //call the repl and return
+      ExpCore.ClassB result= ProgramReduction.allSteps(code3);
+      return new ReplState(this.originalS+"\n"+code, newOriginal,result);
+      }
+    catch(ParseCancellationException parser){
+      System.out.println(parser.getMessage());
+      return null;
+      }
+    catch(ErrorMessage msg){
+      ErrorFormatter.topFormatErrorMessage(msg);
+      return null;
+      } 
+    }
+  }
