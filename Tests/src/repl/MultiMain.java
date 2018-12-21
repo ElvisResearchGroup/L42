d0651061fe8467d240d38a729d9f5a16f4e01c53
marcoServeto <marco.servetto@gmail.com>
2018-12-21 13:38:20 +1300

    Revert "path handling"
    This reverts commit 06079e620fa170fb87e2465906a3e7d31d86fa64.

--- Tests/src/repl/MultiMain.java
+++ Tests/src/repl/MultiMain.java
@@ -1,11 +1,6 @@
 package repl;
-
-import java.nio.file.Paths;
-
 public class MultiMain {
   public static void main(String[] args) throws Throwable {
-    System.setProperty("user.dir",
-        Paths.get(MultiMain.class.getResource("MultiMain.class").getFile().split("!", 2)[0].split(":", 2)[1]).getParent().toString());
     if (args.length > 0) {
       facade.L42.main(args);
     } else {
