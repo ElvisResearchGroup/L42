07ad2a441908e8be337f183ca4cfb9b1e70b1c28
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2019-08-14 10:24:41 +1200

    .

--- L42/srcLombok/is/L42/generated/C.java
+++ L42/srcLombok/is/L42/generated/C.java
@@ -3,3 +3,31 @@ import lombok.Value;
 import lombok.experimental.Wither;
 
 @Value @Wither public class C implements LDom{String inner;}
+
+//TODO:
+/*
+ 
+ replace ctxL with C:LL
+ pTail::= CORE.L | C:LL
+ 
+C calls static{loadclass Init}
+class Init static checks:
+  assertions are on
+  set some global variables for caching and disablyng certain
+    assertions/wf checks
+all ast stuff, including Half implements a single Visitable interface
+  then we have a single big visitor pattern
+  including visit C, visit X and visit List<C> ...
+  
+define Half.E and so on
+
+all toString cutpasted as return Stuff.toS(this) this:Visitable
+also, wf cutpasted as return Stuff.wf(this) and not called automatically
+
+check why toStringVisitor was not great
+
+all wf for C/m/P... uses the parser
+
+subparse for string interpolation and docs
+for each wf criteria, make some negative tests
+*/
