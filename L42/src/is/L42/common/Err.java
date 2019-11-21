f571eed73241f191ecfa26ad19866ae491dfd04f
Claire <foxcoindev@gmail.com>
2019-11-22 10:12:32 +1300

    Resolved overlapping bug in Err.strCmp() and extended it to cover any N number of holes

--- L42/src/is/L42/common/Err.java
+++ L42/src/is/L42/common/Err.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.opentest4j.AssertionFailedError;
@@ -30,38 +31,90 @@ public class Err {
     return res;
     }
 
-  public static boolean strCmp(String complete,String partial){
-    complete=complete.trim();
-    partial=partial.trim();
-    try{assertTrue(Err.strCmpAux(complete,partial));}
-    catch(AssertionFailedError afe){
-      assertEquals(partial,complete);
+  public static boolean strCmp(String cmp1, String cmp2) {
+    cmp1 = cmp1.trim();
+    cmp2 = cmp2.trim();
+    
+    try {
+      assertTrue(strCmpAux(cmp1, cmp2));
+      } catch(AssertionFailedError e) {
+      assertEquals(cmp1, cmp2);
       throw unreachable();
       }
+    
     return true;
     }
-  private static boolean strCmpAux(String complete,String partial){
-    String tmp=partial;
-    int holes=0;
-    List<String> ss=new ArrayList<>();
-    int index=-1;
-    while((index=tmp.indexOf(hole))!=-1){
-      holes+=1;
-      ss.add(tmp.substring(0,index));
-      tmp=tmp.substring(index+hole.length());
-      }
-    ss.add(tmp);
-    if(holes==0){return complete.equals(partial);}
+  
+  private static boolean strCmpAux(String cmp1, String cmp2) {
+    List<String> split = new ArrayList<String>(Arrays.asList(cmp2.split("\\[\\#\\#\\#\\]")));
+    for(int i = 0; i < split.size(); i++) { if(split.get(i).length() == 0) split.remove(i--); }
+    boolean beginswith = cmp2.startsWith(Err.hole);
+    boolean endswith = cmp2.endsWith(Err.hole);
     
-    boolean res=complete.startsWith(ss.get(0)) && complete.endsWith(ss.get(ss.size()-1));
-    if(!res){return false;}
-    if(holes==1){return true;}
-    if(holes==2){
-      complete=complete.substring(ss.get(0).length(),complete.length()-ss.get(2).length());
-      return complete.contains(ss.get(1));
+    int holes = (beginswith ? 1 : 0) + (endswith ? 1 : 0) + split.size() - 1;
+    
+    //Trivial
+    if(holes == 0) { return cmp1.equals(cmp2); }
+    
+    //If we didn't start with a hole, compare everything up to the hole
+    if(!beginswith) {
+      String startCmp2 = split.get(0);
+      
+      //The thing before the hole is bigger than the entire string!
+      if(startCmp2.length() > cmp1.length()) { return false; }
+      
+      String startCmp1 = cmp1.substring(0, startCmp2.length());
+      
+      if(!startCmp1.equals(startCmp2)) { return false; }
+      
+      cmp1 = cmp1.substring(startCmp2.length());
+      split.remove(0);
       }
-    throw todo();
-    }  
+    
+    //If we didn't start with a hole, compare everything up to the hole
+    if(!endswith) {
+      String endCmp2 = split.get(split.size() - 1);
+      
+      //The thing after the hole is bigger than the entire (remaining) string!
+      if(endCmp2.length() > cmp1.length()) { return false; }
+      
+      String endCmp1 = cmp1.substring(cmp1.length() - endCmp2.length(), cmp1.length());
+      
+      if(!endCmp1.equals(endCmp2)) { return false; }
+      
+      cmp1 = cmp1.substring(0, cmp1.length() - endCmp2.length());
+      split.remove(split.size() - 1);
+      }
+    
+    //Everything on both sides of the hole matches, therefore the hole matches everything in the center
+    //Or, alternatively, the single hole was at the end, and therefore everything but the whole matched the beginning or the end of cmp1
+    if(holes == 1) { return true; }
+    
+    //We have a string in between two holes, and all text outside the holes has been removed from both cmp1 and cmp2
+    //Check that the middle text exists within cmp1. If it does, all outer text can be considered part of the holes and we have a match
+    if(holes == 2) { return cmp1.contains(split.get(0)); }
+    
+    if(holes > 2) {
+      //Guess how much space the first hole takes up by iteratively searching for the middle text
+      int index = 0;
+      while((index = cmp1.indexOf(split.get(0))) > -1) {
+        cmp1 = cmp1.substring(index);
+        if(strCmpAux(cmp1, reconstituteRight(split))) { return true; }
+        cmp1 = cmp1.substring(split.get(0).length());
+        }
+      }
+    
+    return false;
+    }
+  
+  private static String reconstituteRight(List<String> list) {
+    StringBuilder builder = new StringBuilder();
+    for(int i = 0; i < list.size(); i++) {
+      builder.append(list.get(i));
+      builder.append(Err.hole);
+      }
+    return builder.toString();
+    }
     
   //errors messages
     public static String duplicatedNameAny(){return
---
+++ L42/src/is/L42/tests/TestHoleComparator.java
@@ -0,0 +1,66 @@
+package is.L42.tests;
+
+import static is.L42.tools.General.todo;
+import static is.L42.tools.General.unreachable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.opentest4j.AssertionFailedError;
+
+import is.L42.common.Err;
+import is.L42.tools.AtomicTest;
+
+public class TestHoleComparator extends AtomicTest.Tester
+{  
+  public static Stream<AtomicTest> test() {
+    return Stream.of(
+        cmp("teststring123", "teststring123", true),
+        cmp("teststring123", "test" + Err.hole + "123", true),
+        cmp("teststring123", "teststring" + Err.hole , true),
+        cmp("teststring123", Err.hole + "string123", true),
+        cmp("teststring123", Err.hole + "string" + Err.hole, true),
+        
+        cmp("teststring123", Err.hole + "teststring1234" + Err.hole, false),
+        cmp("basteststring123", "123stringtestbad", false),
+        cmp("123stringtestbaddd", "123string" + Err.hole + "bad", false),
+        
+        cmp("1234QQABCD", "1234" + Err.hole + "AB" + Err.hole, true),
+        cmp("1234QQABCDQQEFGH", "1234" + Err.hole + "ABCD" + Err.hole + "EFGH", true),
+        cmp("1234QQABCDQQEFGH", Err.hole + "1234" + Err.hole + "ABCD" + Err.hole + "EFGH" + Err.hole, true),
+        cmp("1234ABCDEFGH", "1234" + Err.hole + "ABCD" + Err.hole + "EFGH", true),
+        cmp("1234ABCDEFGH", "1" + Err.hole + "D" + Err.hole + "H", true),
+        
+        cmp("1234ABCDEFGH", Err.hole + "1" + Err.hole + "3" + Err.hole + "C" + Err.hole + "H" + Err.hole, true),
+        cmp("1234ABCDEFGH", Err.hole + "1" + Err.hole + "3" + Err.hole + "H" + Err.hole + "H" + Err.hole, false),
+        cmp("1234ABCDEFGH", Err.hole + "1" + Err.hole + "3" + Err.hole + "N" + Err.hole + "H" + Err.hole, false),
+        cmp("1234ABCDEFGH", Err.hole + "1" + Err.hole + "C" + Err.hole + "3" + Err.hole + "H" + Err.hole, false),
+        
+        cmp("1234QQABCDQQEFGH", Err.hole + "1234A" + Err.hole + "ABCD" + Err.hole + "EFGH" + Err.hole, false),
+        cmp("1234QQABCDQQEFGH", Err.hole + "1234" + Err.hole + "ABCD" + Err.hole + "ENGH" + Err.hole, false),
+        cmp("1234QQABCDQQEFGH", Err.hole + "1274" + Err.hole + "ABCD" + Err.hole + "EFGH" + Err.hole, false),
+        cmp("1234QQABCDQQEFGH", Err.hole + "1234" + Err.hole + "ABFD" + Err.hole + "EFGH" + Err.hole, false),
+        cmp("1234ABCDEFGH", Err.hole + "1234A" + Err.hole + "4ABCD" + Err.hole + "EFGH" + Err.hole, false),
+        cmp("1234ABCD", "1234A" + Err.hole + "4ABCD", false)
+    );
+    }
+  
+  private static AtomicTest cmp(String cmp1, String cmp2, boolean success) {
+    if(success) { return new AtomicTest(() -> { pass(cmp1, cmp2); }); }
+    else { return new AtomicTest(() -> { fail(cmp1, cmp2); }); }
+    }
+
+  private static void pass(String cmp1, String cmp2) {
+    assertTrue(Err.strCmp(cmp1, cmp2));
+    }
+  
+  private static void fail(String cmp1, String cmp2) {
+    assertThrows(AssertionFailedError.class, () -> { Err.strCmp(cmp1, cmp2); });
+    }
+  
+}
