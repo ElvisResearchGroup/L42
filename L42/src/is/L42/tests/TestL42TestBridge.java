980827d4ea6f7131c9d7d8a79bb3c59cea048391
Claire <foxcoindev@gmail.com>
2019-11-25 11:12:55 +1300

    Updated TestL42Bridge to new specification

--- L42/src/is/L42/tests/TestL42TestBridge.java
+++ L42/src/is/L42/tests/TestL42TestBridge.java
@@ -1,116 +1,98 @@
 package is.L42.tests;
 
+import static is.L42.tools.General.L;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TestL42Bridge{
+  static final String separator = "<<<<STRCMP>>>>";
   @MethodSource
   @ParameterizedTest(name = "{index}: {0}")
   public void test(L42Test input) {input.checkPass();}
   public static Stream<L42Test> fromString(String s) { 
-    s += "\n##################################";
-    Stream<String> strings = s.lines();
-    List<L42Test> tests = new ArrayList<>();
-    
-    strings.forEach(new Consumer<String>() {
-
-      L42Test current = null;
-      
-      public void accept(String t) { 
-        if(t.length() == 0 || t.charAt(0) != '#')
-          return;
-        t = t.substring(1);
-        
-        if(isSep(t)) {
-          if(current == null) {
-            current = new L42Test();
-            } else {
-            tests.add(current);
-            current = new L42Test();
-            }
-          return;
-          }
-        
-        if(t.startsWith("PASS") || t.startsWith("FAIL")) {
-          current.pass = t.substring(0, 4).equals("PASS");
-          String[] split = t.split(" ");
-          current.testName = split[split.length - 1];
-          return;
-          }
-        
-        if(t.charAt(0) == '|') {
-          current.message += t.substring(1) + "\n";
-          return;
-          }
-        
-        String[] split = t.split(" ");
-        
-        try {
-          current.lineNumber = Integer.parseInt(split[0]);
-          
-          current.fileName = split[split.length - 1];
-          } catch(NumberFormatException e) {}
+    List<String> strings = L(s.lines());
+    List<L42Test> tests = new ArrayList<>(); 
+    for(String t : strings) {
+      if(t.length() == 0 || t.charAt(0) != '#') { throw new Error("Each line in test descriptor should start with a '#'"); }
+      t = t.substring(1);
+      System.out.println(t);
+      if(isSep(t)) { tests.add(new L42Test()); continue; }
+      else if(t.startsWith("Pass") || t.startsWith("Fail")) {
+        tests.get(tests.size() - 1).pass = t.substring(0, 4).equals("Pass");
+        tests.get(tests.size() - 1).testName = t.substring(t.lastIndexOf(' ') + 1);      
         }
-      
-      boolean isSep(String str) {
-        AtomicBoolean bool = new AtomicBoolean(true);
-        str.chars().forEach((int val) -> {
-          char ascii = (char) val;
-          if(ascii != '#' && ascii != ' ' && ascii != '\n')
-            bool.set(false);
-          });
-        return bool.get();
+      else if(t.startsWith("line:")) {
+        t = t.substring(6);
+        System.out.println(t);
+        tests.get(tests.size() - 1).lineNumber = Integer.parseInt(t.substring(0, t.indexOf(' ')));
+        tests.get(tests.size() - 1).fileName = t.substring(t.lastIndexOf(' ') + 1);   
         }
-    
-      });
-    
+      else if(t.equals("Bool") || t.equals("StrCompare")) { tests.get(tests.size() - 1).type = t; }
+      else if(t.equals("Expected")) { tests.get(tests.size() - 1).message += separator; }
+      else if(t.charAt(0) == '|') { tests.get(tests.size() - 1).message += t.substring(1) + "\n";}
+      }
     return tests.stream();
+    }
+  
+  private static boolean isSep(String str) {
+    for(char c : str.toCharArray()) { 
+      if(c != '#' && c != ' ' && c != '\n') { return false; }
+      }
+    return true;
     }
   }
   
 class L42Test{
   int lineNumber;
+  String type;
   String testName;
   String fileName;
   String message = "";
   boolean pass;
   
   L42Test() {}
-  L42Test(int lineNumber, String fileName, boolean pass) {
-    this.lineNumber=lineNumber;this.fileName=fileName;this.pass=pass;
-    }
   
   public String toString() { return testName + ": line " + lineNumber + " in " + fileName ; }
-  public void checkPass() { 
-    assertTrue(pass, message);
+  public void checkPass() {
+    if(pass) { assertTrue(pass); return; }
+    switch(type) {
+      case "Bool": assertTrue(pass, message);
+      case "StrCompare":
+        String[] expactual = message.split(TestL42Bridge.separator);
+        assertEquals(expactual[1], expactual[0]);
+      }
     }
-  
   }
 
 public class TestL42TestBridge extends TestL42Bridge {
   public static Stream<L42Test> test() {return fromString("""
-###################
-#PASS    TestName
-#45    FileName
-###################
-#FAIL    TestName
-#71    FileName
-#| any text
-#| any text
-#| ...
-#| any text
-###################
-#PASS    TestName
-#42    FileName
-  """);
+###############
+#Pass TestName
+#line: 12   fileName
+###############
+#Fail TestName
+#line: 12   fileName
+#Bool
+#|message
+#|
+###############
+#Fail TestName
+#line: 12   fileName
+#StrCompare
+#Actual
+#|foo(
+#|  bar
+#|  )
+#Expected
+#|expectedme
+""");
   }
 
 }
