44c4a5a69f653f242fa8c3cea510c9ba8f9f3871
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2019-12-07 15:22:03 +1300

    Sum formalism completed. Thinking about nativeTyping

---
+++ L42/src/is/L42/meta/Sum.java
@@ -0,0 +1,27 @@
+package is.L42.meta;
+
+import java.util.List;
+
+import is.L42.common.Program;
+import is.L42.generated.C;
+import is.L42.generated.Core.L;
+
+public class Sum {
+  public static L compose(Program p,L l1, L l2){
+  
+    }
+}
+
+class Plus{
+  public Plus(Program pOut, L topLeft, L topRight, List<C> cs) {
+    this.pOut = pOut;
+    this.topLeft = topLeft;
+    this.topRight = topRight;
+    this.cs = cs;
+    }
+  Program pOut;
+  L topLeft;
+  L topRight;
+  List<C> cs;
+   
+  }
