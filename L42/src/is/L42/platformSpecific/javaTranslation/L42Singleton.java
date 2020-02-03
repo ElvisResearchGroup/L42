19b6efe51e0269c2232e7caa211914d722092dd8
marcoServeto <marco.servetto@gmail.com>
2020-02-04 12:37:09 +1300

    Void is now cachable

---
+++ L42/src/is/L42/platformSpecific/javaTranslation/L42Singleton.java
@@ -0,0 +1,14 @@
+package is.L42.platformSpecific.javaTranslation;
+
+import is.L42.cache.L42Cachable;
+import is.L42.generated.P;
+
+public abstract class L42Singleton<T> implements L42Cachable<T>{
+  @Override public Object[] allFields(){return new Object[0];}
+  @Override public void setField(int i,Object o){throw new IndexOutOfBoundsException();}
+  @Override public Object getField(int i){throw new IndexOutOfBoundsException();}
+  @Override public int numFields(){return 0;}
+  @Override public void setNorm(T t){assert false;}
+  @SuppressWarnings("unchecked")
+  @Override public T myNorm(){return (T)this;} 
+  }
