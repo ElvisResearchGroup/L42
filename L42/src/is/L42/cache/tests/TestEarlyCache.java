30aa6e5c6be87569203364fcd8d12b7c7db0bd97
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2020-02-12 21:29:52 +1300

    .

--- L42/src/is/L42/cache/tests/TestEarlyCache.java
+++ L42/src/is/L42/cache/tests/TestEarlyCache.java
@@ -19,15 +19,21 @@ public class TestEarlyCache {
   }
 class SBox implements L42Cachable<SBox>{
   String s;SBox(String s){this.s=s;}
-  String _methName1(){return this.s+this.s+this.methName2();}
-  String _methName2(){return this.s+this.s+this.methName1();}
+  String _methName1(){
+    return this.s+this.s+new SBox(s).methName1();
+    }
+  String _methName2(){return this.s+this.s/*+new SBox(s).methName1()*/;}
   //generated to be normalizable
   static final Class<SBox> _class=SBox.class;
   private static final L42StandardCache<SBox> myCache=new L42StandardCache<>("SBox",SBox._class);
   static{myCache.lateInitialize(String.class);}
   private SBox norm;
-  boolean started=false; //only if there is any early cache
-  public SBox normed(){return norm!=null?norm:myCache.normalize(this);}
+  boolean out=false;
+  public SBox normed(){
+    if(norm==null){return myCache.normalize(this);}
+    if(norm.out){return norm;}
+    throw new StackOverflowError();
+    }
   @Override public int numFields(){return 1;}
   @Override public Object[] allFields() {return new Object[]{s};}
   @Override public void setField(int i, Object o) { this.s=(String)o;}
@@ -37,21 +43,28 @@ class SBox implements L42Cachable<SBox>{
   @Override public SBox newInstance() { return new SBox(""); }
   @Override public void setNorm(SBox t) {
     assert norm==null;
-    System.err.println(t.s+" Normed with hashcode "+t.hashCode()+" over "+this.hashCode());
+    //System.err.println(t.s+" Normed with hashcode "+t.hashCode()+" over "+this.hashCode());
     //only if there is any early cache, else just "this.norm=t;"
-    if(t!=this){this.norm=t;return;}    
-    if(started){throw new StackOverflowError(s);}
-    started=true;
-    methName1=new CompletableFuture<String>();
-    methName2=new CompletableFuture<String>();
-    CompletableFuture.supplyAsync(t::_methName1).thenAccept(methName1::complete);
-    CompletableFuture.supplyAsync(t::_methName2).thenAccept(methName2::complete);
+    this.norm=t;
+    if(t!=this){return;}
+    CompletableFuture<Void> starter=new CompletableFuture<>();
+    methName1=starter.thenApply(v->t._methName1());
+    methName2=starter.thenApply(v->t._methName2());
     //... more lines, one for each early cache 
-    CompletableFuture.allOf(methName1,methName2).thenAccept(a->this.norm=t);
+    starter.complete(null);
+    CompletableFuture.allOf(methName1,methName2).thenAccept(a->out=true);
     }
   //generated for caching method methName1
   CompletableFuture<String> methName1;  
-  String methName1(){return Util.join(normed().methName1);}
+  String methName1(){
+    var n=normed();
+    try{return n.methName1.join();}
+    catch(CompletionException ce){
+      if(ce.getCause() instanceof RuntimeException){throw (RuntimeException)ce.getCause();}
+      if(ce.getCause() instanceof Error){throw (Error)ce.getCause();}
+      throw ce;
+      }    //return Util.join(normed().methName1);
+    }
   //generated for caching method methName2
   CompletableFuture<String> methName2;  
   String methName2(){return Util.join(normed().methName2);}
