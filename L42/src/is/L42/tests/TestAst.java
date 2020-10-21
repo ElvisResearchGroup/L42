d6a311195a9721d5b2aeb913b2b571240de020ec
marcoServeto <marco.servetto@gmail.com>
2020-10-22 08:43:00 +1300

    case in file name

--- L42/src/is/L42/tests/TestAst.java
+++ L42/src/is/L42/tests/TestAst.java
@@ -9,7 +9,7 @@ import is.L42.platformSpecific.javaTranslation.Resources;
 public class TestAst extends is.L42.tools.TestL42Bridge{
   public static Stream<L42Test> test() throws IOException, URISyntaxException{
     Resources.clearResKeepReuse();
-    var res=TestAst.class.getResource("L42Source/TestAst");
+    var res=TestAst.class.getResource("L42Source/TestAST");
     is.L42.main.Main.main(Paths.get(res.toURI()).toString());
     return fromString(Resources.tests());
     }
